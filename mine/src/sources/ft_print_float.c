fc15a7615765cad263b7c4996525823aec87ddd9
Four Graceford <fgracefo@am-d1.msk.21-school.ru>
2020-08-13 13:03:43 +0300

    693 tests float

--- mine/src/sources/ft_print_float.c
+++ mine/src/sources/ft_print_float.c
@@ -6,7 +6,7 @@
 /*   By: fgracefo <fgracefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 17:42:07 by fgracefo          #+#    #+#             */
-/*   Updated: 2020/08/12 12:38:36 by fgracefo         ###   ########.fr       */
+/*   Updated: 2020/08/13 13:02:36 by fgracefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int		ft_print_float(va_list list,t_flag flag)
 	long long	str_len;
 	int			k;
 
-	number = va_arg(list, double);
+	number = (flag.length.l == 1) ? va_arg(list, long double) : va_arg(list, double);
 
 	str = ft_string_float(number,flag);
 	str_len = ft_strlen(str);
